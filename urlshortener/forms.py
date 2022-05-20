9b275b5f481df6f4de0d245ba2051d974d81348b
ThatsTheSpirit <thatsthespirit@protonmail.ch>
2022-05-20 14:56:33 +0300

    Из стилей убран id heading-h1 Изменена форма регистрации юзера а также ее внешний вид

--- urlshortener/forms.py
+++ urlshortener/forms.py
@@ -4,11 +4,24 @@ from django.contrib.auth.models import User
 
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-control'}))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'autofocus': False})
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        # widgets = {
+        #     'username': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'email': forms.EmailInput(attrs={'class': 'form-control'}),
+        #     'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+        #     'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+        # }
 
 
 class UrlForm(forms.Form):
